f0e834dbd155bc4c2cfedd85171dc66763c28c63
Woongcheol Shin <1994_100@naver.com>
2022-04-07 20:03:49 +0900

    add practice

--- src/2. 함수/실습.ws.kts
+++ src/2. 함수/실습.ws.kts
@@ -2,25 +2,25 @@ import kotlin.math.PI
 
 // 원 넓이 계산 함수
 fun circleArea(radius: Double): Double {
-    return PI*radius*radius
+    return PI * radius * radius
 }
 
 // 식이 본문인 함수
-fun circleAreaTwo(radius:Double): Double = PI*radius*radius
+fun circleAreaTwo(radius: Double): Double = PI * radius * radius
 
 // 식이 본문인 함수 - 반환 타입 생략
-fun circleAreaTwo(radius:Double) = PI*radius*radius
+fun circleAreaTwo(radius: Double) = PI * radius * radius
 
 // 위치 기반 인자
 fun rectangleArea(width: Double, height: Double): Double {
-    return width*height
+    return width * height
 }
 
 // 이름 붙은 인자로 호출
 rectangleArea(width = w, height = h)
 
 // 디폴트 파라미터
-fun readInt(radix:Int = 10) = readLine()!!.toInt(radix)
+fun readInt(radix: Int = 10) = readLine()!!.toInt(radix)
 
 // vararg
 fun printSorted(vararg items: Int) {
@@ -28,3 +28,17 @@ fun printSorted(vararg items: Int) {
     println(items.contentToString())
 }
 
+// 조건문
+fun hexDigit(n: Int): Char {
+    if (n in 0..9) return '0' + n
+    else if (n in 10..15) return 'A' + n - 10
+    else return '?'
+}
+
+fun hexDigitTwo(n: Int): Char {
+    when {
+        n in 0..9 -> return '0' + n
+        n in 10..15 -> return 'A' + n - 10
+        else -> return '?'
+    }
+}
---
+++ src/main/kotlin/test.kt
@@ -0,0 +1,25 @@
+fun main() {
+    fun hexDigit(n: Int): Char {
+        if (n in 0..9) return '0' + n
+        else if (n in 10..15) return 'A' + n - 10
+        else return '?'
+    }
+
+    println(hexDigit(1))
+    println(hexDigit(1).javaClass)
+    println(hexDigit(11))
+    println(hexDigit(11).javaClass)
+
+    fun hexDigitTwo(n: Int): Char {
+        when {
+            n in 0..9 -> return '0' + n
+            n in 10..15 -> return 'A' + n - 10
+            else -> return '?'
+        }
+    }
+
+    println(hexDigitTwo(5))
+    println(hexDigitTwo(5).javaClass)
+    println(hexDigitTwo(15))
+    println(hexDigitTwo(15).javaClass)
+}
